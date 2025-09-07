c53d5ee6a23c59960514e85b5104596188d75a0d
mgrecuccio <marco@MacBook-Pro-di-Marco.local>
2025-09-07 08:00:36 +0200

    LC- Configuring product details view

--- store/views.py
+++ store/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from . models import Category, Product
+
 
 def store(request):
-    return render(request, 'store/store.html')
+    all_products = Product.objects.all()
+    context = {'all_products': all_products}
+    return render(request, 'store/store.html', context)
+
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return {'all_categories': all_categories}
+
+
+def product_details(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {'product': product}
+    return render(request, 'store/product-details.html', context)
