b147091975d2def4cf8b38f134546f7a88555d11
mgrecuccio <marco@MacBook-Pro-di-Marco.local>
2025-09-07 21:17:10 +0200

    LC - Adding cookie consent modal

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
-    path('product/<slug:slug>/', views.product_details, name='product_details')
+    path('product/<slug:slug>/', views.product_details, name='product_details'),
+    
 ]
