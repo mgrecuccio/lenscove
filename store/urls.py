65d37f68d9e86ea0c5dce210f09684c1bcb5e279
mgrecuccio <marco@MacBook-Pro-di-Marco.local>
2025-09-09 00:37:04 +0200

    LC - Adding gallery view and template

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.store, name='store'),
+    path('', views.best_sellers, name='store'),
+    path('gallery', views.gallery, name='gallery'),
     path('product/<slug:slug>/', views.product_details, name='product_details'),
     
 ]
