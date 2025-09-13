290db3eb59473db26de1ee65db3a78b9e4b1e68f
mgrecuccio <marco@MacBook-Pro-di-Marco.local>
2025-09-13 22:44:45 +0200

    LC - Adding the possibility to filter images by category

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.best_sellers, name='store'),
     path('gallery', views.gallery, name='gallery'),
+    path('gallery/<slug:slug>/', views.gallery, name='gallery_by_category'),
     path('product/<slug:slug>/', views.product_details, name='product_details'),
-    
 ]
